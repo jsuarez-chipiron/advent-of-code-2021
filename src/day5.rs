9c2f4206e01e64448b18f6190dd22b93d2fbd7d2
Javier Suarez <jsuarez@salesforce.com>
2021-12-29 12:25:10 +0100

    day5 part1 and part2 ok

--- src/day5.rs
+++ src/day5.rs
@@ -4,21 +4,21 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 struct VentVector {
-    x1: u32,
-    y1: u32,
-    x2: u32,
-    y2: u32,
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Point {
-    x:u32,
-    y:u32,
+    x:i32,
+    y:i32,
 }
 
 #[derive(Debug)]
 struct Board {
-    points: HashMap<Point, u32>,
+    points: HashMap<Point, i32>,
 }
 
 impl FromStr for VentVector {
@@ -28,10 +28,10 @@ impl FromStr for VentVector {
         let line = s.replace(" -> ", ",");
         let coords: Vec<&str> = line.split(',').collect();
 
-        let x1_fromstr = coords[0].parse::<u32>()?;
-        let y1_fromstr = coords[1].parse::<u32>()?;
-        let x2_fromstr = coords[2].parse::<u32>()?;
-        let y2_fromstr = coords[3].parse::<u32>()?;
+        let x1_fromstr = coords[0].parse::<i32>()?;
+        let y1_fromstr = coords[1].parse::<i32>()?;
+        let x2_fromstr = coords[2].parse::<i32>()?;
+        let y2_fromstr = coords[3].parse::<i32>()?;
 
         Ok(VentVector { 
             x1: x1_fromstr, 
@@ -46,6 +46,7 @@ impl VentVector {
     fn get_points(&self) -> Vec<Point> {
         
         let mut ret: Vec<Point> = Vec::new();
+        // println!("vector: {:?}", self);
 
         if self.x1 == self.x2 {
             // vertical line
@@ -54,11 +55,43 @@ impl VentVector {
                 ret.push(Point { x: self.x1, y: i });
             }
         } else {
-            // horizontal line
-            let (first, last) = sort(self.x1, self.x2);
-            // println!("y: {}: {}", first, last);
-            for i in first..=last {
-                ret.push(Point { x: i, y: self.y1 });
+            if self.y1 == self.y2 {
+                // horizontal line
+                let (first, last) = sort(self.x1, self.x2);
+                // println!("y: {}: {}", first, last);
+                for i in first..=last {
+                    ret.push(Point { x: i, y: self.y1 });
+                }
+            } else {
+                // diagonal line
+                let mut i = 0;
+                while i <= (self.x2-self.x1).abs() { 
+                    if self.x1 < self.x2 {
+                        if self.y1 < self.y2 {
+                            // println!("up - up");
+                            // println!("xx: {}-{}-{}", i, self.x1+i, self.y1+i);
+                            ret.push(Point { x: self.x1+i, y: self.y1+i });
+                        }
+                        else {
+                            // println!("up - down");
+                            // println!("xx: {}-{}-{}", i, self.x1+i, self.y1-i);
+                            ret.push(Point { x: self.x1+i, y: self.y1-i });
+                        }
+                    }
+                    else {
+                        if self.y1 < self.y2 {
+                            // println!("down - up");
+                            // println!("xx: {}-{}-{}", i, self.x1-i, self.y1+i);
+                            ret.push(Point { x: self.x1-i, y: self.y1+i });
+                        }
+                        else {
+                            // println!("down - down");
+                            // println!("xx: {}-{}-{}", i, self.x1-i, self.y1-i);
+                            ret.push(Point { x: self.x1-i, y: self.y1-i });
+                        }
+                    }
+                    i+=1;
+                }
             }
         }
 
@@ -66,7 +99,7 @@ impl VentVector {
     }
 }
 
-fn sort(x: u32, y:u32) -> (u32, u32) {
+fn sort(x: i32, y:i32) -> (i32, i32) {
     if x > y {
         return (y, x);
     }
@@ -97,23 +130,15 @@ fn main() {
     let vents_horizontal_and_vertical: Vec<&VentVector> = vents.iter().filter(|v| v.x1 == v.x2 || v.y1 == v.y2).map(|x|x).collect();
 
     let mut board = Board::new();
-
-    // let v = VentVector{x1: 0, y1: 0, x2: 0, y2: 2 };
-    // // let r = v.get_points();
-    // board.add_vent(&v);
-
-    // // println!("{:?}", r);
-
-    // let v = VentVector{x1: 0, y1: 1, x2: 3, y2: 1 };
-    // // let r = v.get_points();
-    // board.add_vent(&v);
-
-    // // println!("{:?}", r);
-    // println!("{:?}", board);
-
     vents_horizontal_and_vertical.iter().for_each(|vv| board.add_vent(vv));
-
     let result = board.points.iter().filter(|&(_, x)| *x > 1).fold(0, |acc, _| acc + 1);
 
     println!("part 1 result: {}", result);
+
+    let mut board = Board::new();
+    vents.iter().for_each(|vv| board.add_vent(vv));
+    let result = board.points.iter().filter(|&(_, x)| *x > 1).fold(0, |acc, _| acc + 1);
+
+
+    println!("part 2 result: {}", result);
 }
