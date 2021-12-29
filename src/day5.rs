4fd93fda2a1a949c01858984555dc3ec33d34ca9
Javier Suarez <jsuarez@salesforce.com>
2021-12-29 11:57:10 +0100

    part 1 day5 increase performance

--- src/day5.rs
+++ src/day5.rs
@@ -1,5 +1,6 @@
 use std::num::ParseIntError;
 use std::str::FromStr;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 struct VentVector {
@@ -9,16 +10,15 @@ struct VentVector {
     y2: u32,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Point {
     x:u32,
     y:u32,
-    num_intersecs: u32,
 }
 
 #[derive(Debug)]
 struct Board {
-    points: Vec<Point>,
+    points: HashMap<Point, u32>,
 }
 
 impl FromStr for VentVector {
@@ -51,14 +51,14 @@ impl VentVector {
             // vertical line
             let (first, last) = sort(self.y1, self.y2);
             for i in first..=last {
-                ret.push(Point { x: self.x1, y: i, num_intersecs: 1 });
+                ret.push(Point { x: self.x1, y: i });
             }
         } else {
             // horizontal line
             let (first, last) = sort(self.x1, self.x2);
             // println!("y: {}: {}", first, last);
             for i in first..=last {
-                ret.push(Point { x: i, y: self.y1, num_intersecs: 1 });
+                ret.push(Point { x: i, y: self.y1 });
             }
         }
 
@@ -75,7 +75,7 @@ fn sort(x: u32, y:u32) -> (u32, u32) {
 
 impl Board {
     fn new() -> Self {
-        Board{points: Vec::new()}
+        Board{points: HashMap::new()}
     }
 
     fn add_vent(&mut self, vv: &VentVector) {
@@ -87,13 +87,7 @@ impl Board {
     }
 
     fn add_vent_point(&mut self, vp: Point) {
-        for bp in self.points.iter_mut() {
-            if bp.x == vp.x && bp.y == vp.y {
-                bp.num_intersecs += 1;
-                return;
-            }
-        }
-        self.points.push(vp);
+        *self.points.entry(vp).or_insert(0) += 1;
     }
 }
 
@@ -119,6 +113,7 @@ fn main() {
 
     vents_horizontal_and_vertical.iter().for_each(|vv| board.add_vent(vv));
 
-    let result = board.points.iter().filter(|x| x.num_intersecs > 1).fold(0, |acc, _| acc + 1);
+    let result = board.points.iter().filter(|&(_, x)| *x > 1).fold(0, |acc, _| acc + 1);
+
     println!("part 1 result: {}", result);
 }
