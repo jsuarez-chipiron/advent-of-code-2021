056cb16cbaf57f6c71fcffff5f6ffbbd925313e1
Javier Suarez <jsuarez@salesforce.com>
2021-12-31 20:58:05 +0100

    day10 part2 ok

--- src/day10.rs
+++ src/day10.rs
@@ -38,19 +38,54 @@ fn validate_line(line: &String) -> Result<(), char> {
     Ok(())
 }
 
+fn complete(line: &String) -> Vec<char> {
+    let mut stack: VecDeque<char> = VecDeque::new();
+    for i in line.chars() {
+        match i {
+            ']' | ')' | '>' | '}' => {
+                let _ = stack.pop_front().unwrap();
+            },
+            '[' | '(' | '<' | '{' => {
+                stack.push_front(i);
+            },
+            _ => {},
+        };
+    }
+
+    let ret: Vec<char> = stack.iter().map(|&i| {
+        match i {
+            '[' => ']',
+            '(' => ')',
+            '{' => '}',
+            '<' => '>',
+            _ => i,
+        }
+    }).collect();
+
+    ret
+
+}
+
 fn main() {
-    let mut dict: HashMap<char, u32> = HashMap::new();
+    let mut dict: HashMap<char, u64> = HashMap::new();
 
     dict.insert(')', 3);
     dict.insert(']', 57);
     dict.insert('}', 1197);
     dict.insert('>', 25137);
 
+    let mut dict2: HashMap<char, u64> = HashMap::new();
+
+    dict2.insert(')', 1);
+    dict2.insert(']', 2);
+    dict2.insert('}', 3);
+    dict2.insert('>', 4);
+
     let raw = aoc::read_one_per_line::<String>("inputs/day10.txt").unwrap();
-    let raw: Vec<&String> = raw.iter().take(raw.len()-1).collect();
+    let lines: Vec<&String> = raw.iter().take(raw.len()-1).collect();
 
     let mut errors: Vec<char> = Vec::new();
-    raw.iter().for_each(|i| {
+    lines.iter().for_each(|i| {
         match validate_line(*i) {
             Ok(()) => {},
             Err(c) => {
@@ -59,5 +94,37 @@ fn main() {
         }
     });
 
-    println!("Part1: {}", errors.iter().map(|i| dict[i]).sum::<u32>());
+    println!("Part1: {}", errors.iter().map(|i| dict[i]).sum::<u64>());
+
+    let corrects: Vec<&String> = raw.iter().filter(|&i| {
+        if i.len() == 0 {
+            return false;
+        }
+        if let Ok(()) = validate_line(i) {
+            return true;
+        }
+        false
+    }).collect();
+
+    let mut completes: Vec<Vec<char>> = Vec::new();
+
+    corrects.iter().for_each(|i| {
+        // let complete = complete(*i);
+        completes.push(complete(*i));
+    });
+
+    let mut results: Vec<u64> = Vec::new();
+    for line in completes {
+        let mut score = 0;
+        for c in line {
+            score *= 5;
+            score += dict2[&c];
+        }
+        results.push(score);
+    }
+
+    results.sort();
+
+    println!("Part 2: {}", results[results.len()/2]);
+
 }
