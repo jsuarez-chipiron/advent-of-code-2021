4e5f0a731a6e873ec18d10f9833bb919861f9ba1
Javier Suarez <jsuarez@salesforce.com>
2022-01-03 18:34:41 +0100

    day12 part2 ok

--- src/day12.rs
+++ src/day12.rs
@@ -1,7 +1,7 @@
 use std::collections::{HashSet, HashMap};
 
 fn parse_input() -> Vec<HashSet<String>> {
-    let raw = aoc::read_one_per_line::<String>("inputs/day12_test.txt").unwrap();
+    let raw = aoc::read_one_per_line::<String>("inputs/day12.txt").unwrap();
 
     let mut ret: Vec<HashSet<String>> = Vec::new();
 
@@ -22,27 +22,25 @@ fn is_small_cave(cave: &String) -> bool {
     cave.to_lowercase() == *cave
 }
 
-fn has_visited_cave(cave: &String, current_path: &Vec<String>) -> bool {
-    let mut ret = false;
-    current_path.iter().for_each(|i| {
-        if i == cave {
-            ret = true;
-        }
-    });
-    ret
-}
-
 fn num_of_twos(mapa: &HashMap<&str, u8>) -> u8 {
     let mut count = 0u8;
     mapa.iter().for_each(|(_, &v)| {
-        if v != 1 {
+        if v == 2 {
             count += 1;
         }
     });
     count
 }
 
-fn can_continue(_cave: &String, current_path: &Vec<String>) -> bool {
+fn can_continue(cave: &String, current_path: &Vec<String>) -> bool {
+
+    if cave == "start" {
+        return false;
+    }
+
+    if !is_small_cave(cave) {
+        return true;
+    }
 
     let mut mapa: HashMap<&str, u8> = HashMap::new();
 
@@ -51,25 +49,26 @@ fn can_continue(_cave: &String, current_path: &Vec<String>) -> bool {
             *mapa.entry(i).or_insert(0) += 1;
         }
     });
-
-    if num_of_twos(&mapa) == 1 {
-        return false;
+     
+    match mapa.get(&cave[..]) {
+        Some(&v) => {
+            if v == 0 {
+                return true;
+            }
+            else {
+                if num_of_twos(&mapa) == 0 {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        None => {
+            return true;
+        },
     }
-    true
-    // *mapa.entry(&cave).or_insert(0) += 1;
-    
 
-    // println!("six: {}", cave);
-    // println!("xx: {:?}", mapa);
-
-
-    // let mut ret = false;
-    // current_path.iter().for_each(|i| {
-    //     if i == cave {
-    //         ret = true;
-    //     }
-    // });
-    // ret
 }
 
 fn get_next_caves (cave: &String, caves: &Vec<HashSet<String>>) -> Vec<String> {
@@ -99,23 +98,14 @@ fn new_from_current_path(cave: &String, old_path: Vec<String>) -> Vec<String> {
 
 fn run_paths(current_cave: &String, caves: &Vec<HashSet<String>>, current_path: Vec<String>, founded_paths: &mut Vec<Vec<String>>, level: u8) {
 
-    // println!("current_cave [{}]: {}", level, current_cave);
     let current_path = new_from_current_path(current_cave, current_path);
     if current_cave == "end" {
         founded_paths.push(current_path);
     }
     else {
         let next_caves = get_next_caves(current_cave, caves);
-        // println!("next_caves [{}]: {:?}", level, next_caves);
         for next_cave in next_caves {
-            // println!("next_cave [{}]: {}", level, next_cave);
-
-            if is_small_cave(&next_cave) {
-                if can_continue(&next_cave, &current_path) && next_cave != "start" {
-                    run_paths(&next_cave, caves, current_path.clone(), founded_paths, level+1);
-                }
-            } 
-            else {
+            if can_continue(&next_cave, &current_path) && next_cave != "start" {
                 run_paths(&next_cave, caves, current_path.clone(), founded_paths, level+1);
             }
         }
@@ -131,7 +121,7 @@ fn main() {
 
     run_paths(&starting_position, &caves, current_path, &mut founded_paths, 0);
 
-    println!("Part 1: {}", founded_paths.len());
+    println!("Part 2: {}", founded_paths.len());
 
 
 }
