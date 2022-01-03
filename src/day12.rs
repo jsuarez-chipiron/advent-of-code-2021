fe78efd7ef8400404a57bbd15a50acbc28852030
Javier Suarez <jsuarez@salesforce.com>
2022-01-03 14:36:18 +0100

    day12 part1 ok

--- src/day12.rs
+++ src/day12.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
 fn parse_input() -> Vec<HashSet<String>> {
-    let raw = aoc::read_one_per_line::<String>("inputs/day12_test.txt").unwrap();
+    let raw = aoc::read_one_per_line::<String>("inputs/day12.txt").unwrap();
 
     let mut ret: Vec<HashSet<String>> = Vec::new();
 
@@ -18,21 +18,19 @@ fn parse_input() -> Vec<HashSet<String>> {
     ret
 }
 
-fn is_big_cave(cave: &String) -> bool {
-    cave.to_uppercase() == *cave
+fn is_small_cave(cave: &String) -> bool {
+    cave.to_lowercase() == *cave
 }
 
-// fn get_next_steps<'a> (cave: &String, caves: &'a Vec<HashSet<String>>) -> Vec<&'a HashSet<String>> {
-//     let mut ret: Vec<&HashSet<String>> = Vec::new();
-
-//     caves.iter().for_each(|i| {
-//         if i.contains(cave) {
-//             ret.push(i);
-//         }
-//     });
-
-//     ret
-// }
+fn has_visited_cave(cave: &String, current_path: &Vec<String>) -> bool {
+    let mut ret = false;
+    current_path.iter().for_each(|i| {
+        if i == cave {
+            ret = true;
+        }
+    });
+    ret
+}
 
 fn get_next_caves (cave: &String, caves: &Vec<HashSet<String>>) -> Vec<String> {
     let mut ret: Vec<String> = Vec::new();
@@ -50,38 +48,50 @@ fn get_next_caves (cave: &String, caves: &Vec<HashSet<String>>) -> Vec<String> {
     ret
 }
 
-fn run_paths(current_cave: &String, caves: &Vec<HashSet<String>>, visited_caves: &mut HashSet<String>, founded_paths: &mut Vec<String>) {
-    founded_paths.push(current_cave.clone());
-    let next_caves = get_next_caves(current_cave, caves);
-    for next_cave in next_caves {
-        if next_cave == "end" {
-            println!("TERMINE");
-        }
-        else {
-            if !is_big_cave(&next_cave) && !visited_caves.contains(&next_cave) {
-                visited_caves.insert(next_cave.clone());
-                run_paths(&next_cave, caves, visited_caves, founded_paths);
+fn new_from_current_path(cave: &String, old_path: Vec<String>) -> Vec<String> {
+    let mut ret: Vec<String> = Vec::new();
+    old_path.iter().for_each(|i| {
+        ret.push(i.to_string());
+    });
+    ret.push(cave.to_string());
+    ret
+}
+
+fn run_paths(current_cave: &String, caves: &Vec<HashSet<String>>, current_path: Vec<String>, founded_paths: &mut Vec<Vec<String>>, level: u8) {
+
+    // println!("current_cave [{}]: {}", level, current_cave);
+    let current_path = new_from_current_path(current_cave, current_path);
+    if current_cave == "end" {
+        founded_paths.push(current_path);
+    }
+    else {
+        let next_caves = get_next_caves(current_cave, caves);
+        // println!("next_caves [{}]: {:?}", level, next_caves);
+        for next_cave in next_caves {
+            // println!("next_cave [{}]: {}", level, next_cave);
+
+            if is_small_cave(&next_cave) {
+                if !has_visited_cave(&next_cave, &current_path) {
+                    run_paths(&next_cave, caves, current_path.clone(), founded_paths, level+1);
+                }
+            } 
+            else {
+                run_paths(&next_cave, caves, current_path.clone(), founded_paths, level+1);
             }
         }
     }
-
-     println!("visited_caves: {:?}", visited_caves);
-     println!("founded_paths: {:?}", founded_paths);
 }
 
 fn main() {
     let caves = parse_input();
-    println!("{:?}", caves);
 
-    let mut visited_caves: HashSet<String> = HashSet::new();
-    let mut founded_paths: Vec<String> = Vec::new();
-
+    let mut founded_paths: Vec<Vec<String>> = Vec::new();
     let starting_position = String::from("start");
+    let current_path: Vec<String> = Vec::new();
 
-    let from_start = get_next_caves(&starting_position, &caves);
-    println!("{:?}", from_start);
+    run_paths(&starting_position, &caves, current_path, &mut founded_paths, 0);
 
-    run_paths(&starting_position, &caves, &mut visited_caves, &mut founded_paths);
+    println!("Part 1: {}", founded_paths.len());
 
 
 }
