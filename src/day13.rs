9aaa6a945923cef82ebfc49013ab536c60526cc2
Javier Suarez <jsuarez@salesforce.com>
2022-01-04 14:50:13 +0100

    day13 part1 ok

--- src/day13.rs
+++ src/day13.rs
@@ -1,10 +1,10 @@
 use std::num::ParseIntError;
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Point {
-    x: u32,
-    y: u32,
+    x: usize,
+    y: usize,
 }
 
 #[derive(Debug)]
@@ -19,8 +19,8 @@ impl FromStr for Point {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let coords: Vec<&str> = s.split(',').collect();
 
-        let x1_fromstr = coords[0].parse::<u32>()?;
-        let y1_fromstr = coords[1].parse::<u32>()?;
+        let x1_fromstr = coords[0].parse::<usize>()?;
+        let y1_fromstr = coords[1].parse::<usize>()?;
 
         Ok(Point { 
             x: x1_fromstr, 
@@ -53,29 +53,138 @@ impl FromStr for Fold {
     }
 }
 
-fn parse_coord_input() -> Vec<Point> {
-    aoc::read_one_per_line::<Point>("inputs/day13_test.txt").unwrap()
+fn parse_coord_input(path: &str) -> Vec<Point> {
+    aoc::read_one_per_line::<Point>(path).unwrap()
 }
 
-fn parse_fold_input() -> Vec<Fold> {
-    aoc::read_one_per_line::<Fold>("inputs/day13_test.txt").unwrap()
+fn parse_fold_input(path: &str) -> Vec<Fold> {
+    aoc::read_one_per_line::<Fold>(path).unwrap()
 }
 
-fn get_num_rows(points: &Vec<Point>) -> u32 {
-    points.iter().map(|i| i.y ).max().unwrap()
+fn init_fixed(rows: usize, cols: usize) -> Vec<Vec<u32>> {
+    let mut ret: Vec<Vec<u32>> = Vec::new();
+
+    for _ in 0..rows {
+        ret.push(vec![0; cols]);
+    }
+
+    ret
 }
 
-fn get_num_cols(points: &Vec<Point>) -> u32 {
-    points.iter().map(|i| i.x ).max().unwrap()
+fn convert_to_fixed(points: Vec<Point>) -> Vec<Vec<u32>> {
+    let rows = get_num_rows(&points) as usize;
+    let cols = get_num_cols(&points) as usize;
+
+    let mut ret = init_fixed(rows, cols);
+
+    points.iter().for_each(|i| {
+        ret[i.y][i.x] = 1;
+    });
+    
+    ret
+}
+
+#[allow(dead_code)]
+fn print_fixed(points: &Vec<Vec<u32>>) {
+    for i in 0..points.len() {
+        for j in 0..points[0].len() {
+            print!("{} ", points[i][j]);
+        }
+        println!();
+    }
+}
+
+fn get_num_rows(points: &Vec<Point>) -> usize {
+    points.iter().map(|i| i.y ).max().unwrap() + 1
+}
+
+fn get_num_cols(points: &Vec<Point>) -> usize {
+    points.iter().map(|i| i.x ).max().unwrap() + 1
+}
+
+fn unenumerate(v: Vec<(usize, &Vec<u32>)>) -> Vec<Vec<u32>> {
+    v.iter().map(|(_,x)| x.clone().clone()).collect()
+}
+
+fn reverse_horizontally(v: Vec<Vec<u32>>) -> Vec<Vec<u32>> {
+    v.iter().rev().map(|i| i.clone()).collect()
+}
+
+fn process_fold(p: &u32, points: Vec<Vec<u32>>) -> Vec<Vec<u32>> {
+    let (up, down): (Vec<(usize, &Vec<u32>)>, Vec<(usize, &Vec<u32>)>) = points.iter().enumerate()
+                     .filter(|(i, _)| *i!=*p as usize )
+                     .partition(|(i, _)| *i<*p as usize );
+
+    let up = unenumerate(up);
+    let down = unenumerate(down);
+    let down = reverse_horizontally(down);
+
+    return sum_chunks(up, down)
+}
+
+fn sum_chunks(part1: Vec<Vec<u32>>, part2: Vec<Vec<u32>>) -> Vec<Vec<u32>> {
+    let mut ret: Vec<Vec<u32>> = init_fixed(part1.len(), part1[0].len());
+    for i in 0..part1.len() {
+        for j in 0..part1[0].len() {
+            ret[i][j] = part1[i][j] + part2[i][j];
+        }
+    }
+
+    ret
+}
+
+fn transpose(v: Vec<Vec<u32>>) -> Vec<Vec<u32>> {
+    let mut ret: Vec<Vec<u32>> = init_fixed(v[0].len(), v.len());
+
+    for i in 0..ret.len() {
+        for j in 0..ret[0].len() {
+            ret[i][j] = v[j][i];
+        }
+    }
+
+    ret
+}
+
+
+fn fold_fixed(points: Vec<Vec<u32>>, fold: &Fold) -> Vec<Vec<u32>> {
+    match fold {
+        Fold::Y(p) => {
+            process_fold(p, points)
+        },
+        Fold::X(p) => {
+            let temp_points = process_fold(p, transpose(points));
+            transpose(temp_points)
+        },
+    }
+}
+
+fn count_points(points: Vec<Vec<u32>>) -> usize {
+    let mut ret = 0;
+    points.iter().for_each(|i| {
+        ret += i.iter().filter(|&x| *x>0 ).count();
+    });
+
+    ret
 }
 
 fn main() {
-    let points = parse_coord_input();
-    println!("{:?}", points);
+    let path = "inputs/day13.txt";
 
-    let folds = parse_fold_input();
-    println!("{:?}", folds);
+    let points = parse_coord_input(path);
+    let folds = parse_fold_input(path);
 
-    println!("max cols: {}", get_num_cols(&points));
-    println!("max rows: {}", get_num_rows(&points));
+    let points = convert_to_fixed(points);
+    // print_fixed(&points);
+
+    // for i in folds {
+    //     points = fold_fixed(points, &i);
+    //     // println!();
+    //     // print_fixed(&points);
+    // }
+    let first_fold = fold_fixed(points, &folds[0]);
+
+    let count = count_points(first_fold);
+    println!("Part 1: {}", count);
+
+    
 }
