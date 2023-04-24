72f78a4bea2cb77c11ea8636ff0a0b4e18cbcc45
Javier Suarez <jsuarez@salesforce.com>
2023-04-24 23:24:08 +0200

    refactor regular for with iterators

--- src/day1.rs
+++ src/day1.rs
@@ -6,13 +6,20 @@ fn main()
 
     let mut counter = 0;
 
-    for i in 1..input.len() {
-        // println!("{}", &input[i]);
-        if &input[i] > &input[i - 1] {
-            counter += 1;
-        }
-    }
-    println!("Part 1: the result is {}", counter);
+    // for i in 1..input.len() {
+    //     // println!("{}", &input[i]);
+    //     if &input[i] > &input[i - 1] {
+    //         counter += 1;
+    //     }
+    // }
+    // println!("Part 1: the result is {}", counter);
+
+    // counter = 0;
+    // input.windows(2).for_each(|i| { if i[1] > i[0] { counter += 1; } });
+    // println!("Part 1: the result is {}", counter);
+
+    let result = input.windows(2).filter(|i| { i[1] > i[0] }).count();
+    println!("Part 1: the result is {}", result);
 
     let mut new_input = Vec::new();
 
