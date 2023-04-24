970db9322a494e625281a37eae7e0d6f47d80be6
Javier Suarez <jsuarez@salesforce.com>
2023-04-24 23:39:29 +0200

    refactor regular for with iterators (part2)

--- src/day1.rs
+++ src/day1.rs
@@ -1,10 +1,8 @@
-use aoc;
-
 fn main()
 {
     let input = aoc::read_one_per_line::<u32>("./inputs/day1.txt").unwrap();
 
-    let mut counter = 0;
+    // let mut counter = 0;
 
     // for i in 1..input.len() {
     //     // println!("{}", &input[i]);
@@ -18,23 +16,30 @@ fn main()
     // input.windows(2).for_each(|i| { if i[1] > i[0] { counter += 1; } });
     // println!("Part 1: the result is {}", counter);
 
-    let result = input.windows(2).filter(|i| { i[1] > i[0] }).count();
-    println!("Part 1: the result is {}", result);
+    let result_part1 = input.windows(2).filter(|i| { i[1] > i[0] }).count();
+    println!("Part 1: the result is {}", result_part1);
 
-    let mut new_input = Vec::new();
+    // let mut new_input = Vec::new();
 
-    for i in 1..input.len() - 1 {
-        let new_item = &input[i - 1] + &input[i] + &input[i + 1];
-        new_input.push(new_item);
-    }
+    // for i in 1..input.len() - 1 {
+    //     let new_item = input[i - 1] + input[i] + input[i + 1];
+    //     new_input.push(new_item);
+    // }
 
-    let mut counter = 0;
+    // let mut counter = 0;
 
-    for i in 1..new_input.len() {
-        // println!("{}", &input[i]);
-        if &new_input[i] > &new_input[i - 1] {
-            counter += 1;
-        }
-    }
-    println!("Part 2: the result is {}", counter);
+    // for i in 1..new_input.len() {
+    //     // println!("{}", &input[i]);
+    //     if new_input[i] > new_input[i - 1] {
+    //         counter += 1;
+    //     }
+    // }
+    // println!("Part 2: the result is {}", counter);
+
+    let result_part2 = input.windows(3).fold(Vec::<u32>::new(), |mut a, i| {
+        a.push(i.iter().sum());
+        a
+    }).windows(2).filter(|i| { i[1] > i[0] }).count();
+
+    println!("Part 2: the result is {}", result_part2);
 }
